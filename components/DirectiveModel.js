129ddac4e4ad131a12a47e786c1ffb2c111d6fb2
Marcelino Mejia <mmejia1@tecbaco.com>
2020-10-29 16:54:15 -0500

    terminar el v-model

--- components/DirectiveModel.js
+++ components/DirectiveModel.js
@@ -2,19 +2,22 @@ let DirectiveModel = {
     template: `
         <div>
             <h1 v-text="title" ></h1>
-            <p v-show="mostrar" v-html="messege"></p>
-            <p v-show="user.permission"> EL usuario Tiene permiso para ver esto </p>
-            <p v-show="!user.permission"> EL usuario Tiene No permiso para ver esto </p>
+            <p v-html="messege"></p>
+            <p v-text="input" ></p>
+            <input type="text" v-model="input">
+
+            <h2>Tipo CheckBok</h2>
+            <label>
+                <input type="checkbox" v-model="checked">
+            </label>
         </div>
     `,
     data (){
         return {
             title    : 'Directivas v-model',
             messege  : '<b>Hola desde directive v-model</b>',
-            mostrar  : true,
-            user     : {
-                permission : true
-            } 
+            input    : 'Data del Input',
+            checked  : false
         }
     }
 }
